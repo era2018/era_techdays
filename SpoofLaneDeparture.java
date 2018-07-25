6e7f151bb2681f2c73c9c43c5ef0b1e4b0a513db
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-07-25 11:28:05 -0400

    Code to directly insert events into the database in case of problems with either the hardware or the LittleBit server

---
+++ SpoofLaneDeparture.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class SpoofLaneDeparture {
+    public static void main(String[] args) {
+        try {
+        	Connection connection = DriverManager.getConnection("jdbc:sybase:Tds:10.171.1.252:2638/ubdataba", "ubdb", "123456");
+        	String testInsert = "INSERT INTO eventLog (eventID, carID, logTime) VALUES (1, '243c201f7957', NOW())";
+        	PreparedStatement ps = connection.prepareStatement(testInsert);
+        	ps.execute();
+        	testInsert = "INSERT INTO eventLog (eventID, carID, logTime) VALUES (1, 'Spoof', NOW())";
+        	ps = connection.prepareStatement(testInsert);
+        	ps.execute();
+            connection.close();
+        }	
+        catch (Exception e) {
+           e.printStackTrace(System.out);
+        }
+    }
+}
