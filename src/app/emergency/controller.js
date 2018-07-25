95878fd1bf2b3d083407eef697e5c2955dd867f4
Spare <Spare@era2018.com>
2018-07-25 14:36:18 -0400

    Added Date and Time to javascript controller

--- src/app/emergency/controller.js
+++ src/app/emergency/controller.js
@@ -4,18 +4,20 @@ $(document).ready(function() {
 			url: "http://localhost:8080/event"
 		}).then(function(data){ 
 			console.log(data);
-			console.log(data.eventCode);
+			console.log(data.eventCode)
+			var d = new Date();
+			console.log(+d.getFullYear()+" "+(d.getMonth()+1)+" "+d.getHours()+" "+d.getMinutes)
 			if(data.eventCode == 1){
-				document.getElementById('data').innerHTML = "Betty Off Road";
+				document.getElementById('data').innerHTML = "Betty Off Road  "+d.getFullYear()+":"+(d.getMonth()+1)+":"+d.getHours()+":"+d.getMinutes();
 			}
 		    else if(data.eventCode == 2){
-				document.getElementById('data').innerHTML = "Betty in traffic jam";
+				document.getElementById('data').innerHTML = "Betty in traffic jam  "+d.getFullYear()+":"+(d.getMonth()+1)+":"+d.getHours()+":"+d.getMinutes();
 			}
 			else if(data.eventCode== 3){
-				document.getElementById('data').innerHTML = "Betty in fender bender";
+				document.getElementById('data').innerHTML = "Betty in fender bender  "+d.getFullYear()+":"+(d.getMonth()+1)+":"+d.getHours()+":"+d.getMinutes();
 			}
 			else{
-				document.getElementById('data').innerHTML = "Lane Departure";
+				document.getElementById('data').innerHTML = "Lane Departure  "+d.getFullYear()+":"+(d.getMonth()+1)+":"+d.getHours()+":"+d.getMinutes();
 					
 			}
 			
