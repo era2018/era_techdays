1a3b3630402b2e176f60bbc19aa8200c541162d8
Julia Gallagher <dottiespottie12321@gmail.com>
2018-07-26 11:07:49 -0400

    Edited Syntax and grammer

--- src/app/emergency/controller.js
+++ src/app/emergency/controller.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function control(){
 $(document).ready(function() {
 		$.ajax({
@@ -27,6 +28,41 @@ $(document).ready(function() {
 
 	});
 	setTimeout(control, 5000);
+=======
+var lastEvent = 0;
+var list = "";
+var alerts = {};
+
+alerts[1] = "Betty swerved off the road";
+alerts[2] = "Betty is in a traffic jam";
+alerts[3] = "Betty was involved in a fender bender";
+
+function display(data)
+{ 
+	if(data.eventCode != lastEvent) {
+		lastEvent = data.eventCode;
+		
+		if(data.eventCode >= 1 && data.eventCode <= 4 ) {
+			var date = new Date();
+			str = alerts[data.eventCode] + date.toLocaleString() + "</br>";
+			//date.getFullYear()+"-"+(date.getMonth()+1)+" "+date.getHours()+":"+date.getMinutes() +"</br>";
+			
+			list = str + list;
+			document.getElementById('data').innerHTML = list
+		}		
+	}
+	console.log(data);
+	console.log(data.eventCode)
+}
+
+function control() {
+    $(document).ready(function() {
+        $.ajax({
+                url: "http://localhost:8080/event"
+        }).then(display);
+    });
+	setTimeout(control, 500);
+>>>>>>> Stashed changes
 }
 
 control();
