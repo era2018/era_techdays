0ca5a2cc23b0a2f024b7a2b8e507e27ec8854160
uddalokbiswas <Uddalok.Biswas@era2018.onmicrosoft.com>
2018-07-26 10:13:14 -0400

    changed controller.js

---
+++ src/app/ride-share-go/controller.js
@@ -0,0 +1,32 @@
+function control(){
+            var counter = 0;
+            var counter2 = 0;
+            $(document).ready(function() {
+                    $.ajax({
+                        url: "http://localhost:8080/event"
+                    }).then(function(data) {
+                        console.log(data.event);
+                        if(data.event == 4 && counter == 0){
+                            document.getElementById('data').innerHTML = "Betty got picked up";
+                            counter++;
+                        }
+                        else if (data.event == 5 && counter2 == 0) {
+                            document.getElementById('data').innerHTML = "All passengers have been picked up";
+                            counter2++;
+                        }
+                        else if(data.event == 4 && counter == 1){
+                            document.getElementById('data').innerHTML = "Entering Deadzone";
+                            counter++;
+                        }
+                        else if(data.event == 5 && counter2 == 1){
+                            document.getElementById('data').innerHTML = "Exiting Deadzone";
+                            counter2++;
+                        }
+                        else if(data.event== 5 && counter2 == 2){
+                            document.getElementById('data').innerHTML = "Betty got dropped off";
+                        }
+                    });
+            });
+            setTimeout(control, 3000);
+        }
+ control();
